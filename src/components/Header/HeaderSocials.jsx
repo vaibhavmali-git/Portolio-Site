7d8055242d5b23eaf43c8f0b54a7ce94ace8580b
VaibhavMali001 <malivaibhav.7620@gmail.com>
2024-03-09 13:30:34 +0530

    changes made

--- src/components/Header/HeaderSocials.jsx
+++ src/components/Header/HeaderSocials.jsx
@@ -7,8 +7,8 @@ import { BsTwitter } from "react-icons/bs";
 function HeaderSocials() {
   return (
     <div className='header__socials'>
-      <a href="https://linkedin.com/in/vaibhav-mali-841779218" target='_blank'><BsLinkedin /></a>
-      <a href="https://github.com/VaibhavMali001" target='_blank'><BsGithub /></a>
+      <a href="https://www.linkedin.com/in/vaibhavmali-li/" target='_blank'><BsLinkedin /></a>
+      <a href="https://github.com/vaibhavmali-git" target='_blank'><BsGithub /></a>
       <a href="https://twitter.com/vaibhavmali_01" target='_blank'><BsTwitter  /></a>
     </div>
   )
