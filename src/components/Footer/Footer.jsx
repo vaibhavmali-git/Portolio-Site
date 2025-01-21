dbadde22f5c608a7f0c90229ea173e4226855626
Vaibhav Mali <malivaibhav.7620@gmail.com>
2025-01-21 10:47:49 +0530

    minor changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,33 +1,71 @@
 import React from "react";
 import "./footer.css";
-import {BsLinkedin} from 'react-icons/bs'
-import {BsGithub} from 'react-icons/bs'
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
 import { BsTwitter } from "react-icons/bs";
-import {BsDribbble} from 'react-icons/bs'
+import { BsDribbble } from "react-icons/bs";
 
 function Footer() {
   return (
     <footer>
-      <a href="#" className="footer__logo a-hover">&lt;Vaibhav<code>/&gt;</code></a>
-     <ul className="permalinks"> 
-      <li><a className="a-hover" href="#"><span>#Home</span></a></li>
-      <li><a className="a-hover" href="#about"><span>#About</span></a></li>
-      <li><a className="a-hover" href="#skills"><span>#Skills</span></a></li>
-      <li><a className="a-hover" href="#services"><span>#Education</span></a></li>
-      <li><a className="a-hover" href="#portfolio"><span>#Portfolio</span></a></li>
-      <li><a className="a-hover" href="#contact"><span>#Contact</span></a></li>
-</ul>
+      <a href="#" className="footer__logo a-hover">
+        &lt;Vaibhav<code>/&gt;</code>
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a className="a-hover" href="#">
+            <span>#Home</span>
+          </a>
+        </li>
+        <li>
+          <a className="a-hover" href="#about">
+            <span>#About</span>
+          </a>
+        </li>
+        <li>
+          <a className="a-hover" href="#skills">
+            <span>#Skills</span>
+          </a>
+        </li>
+        <li>
+          <a className="a-hover" href="#services">
+            <span>#Education</span>
+          </a>
+        </li>
+        <li>
+          <a className="a-hover" href="#portfolio">
+            <span>#Portfolio</span>
+          </a>
+        </li>
+        <li>
+          <a className="a-hover" href="#contact">
+            <span>#Contact</span>
+          </a>
+        </li>
+      </ul>
       <div className="footer__socials">
-      <a href="https://linkedin.com/in/vaibhav-mali-841779218" target='_blank'><BsLinkedin size={16}/></a>
-      <a href="https://github.com/VaibhavMali001" target='_blank'><BsGithub size={16}/></a>
-      <a href="https://twitter.com/vaibhavmali_01" target='_blank'><BsTwitter   size={17}/></a>
+        <a
+          href="https://linkedin.com/in/vaibhav-mali-841779218"
+          target="_blank"
+        >
+          <BsLinkedin size={16} />
+        </a>
+        <a href="https://github.com/VaibhavMali001" target="_blank">
+          <BsGithub size={16} />
+        </a>
+        <a href="https://twitter.com/vaibhavmali_01" target="_blank">
+          <BsTwitter size={17} />
+        </a>
       </div>
 
       <div className="footer__copyright">
-        <small>&copy; <span className="footer-name">Vaibhav Mali.</span> All rights reserved</small>
+        <small>
+          &copy; <span className="footer-name">Vaibhav Mali.</span> All rights
+          reserved
+        </small>
       </div>
     </footer>
-  )
+  );
 }
 
 export default Footer;
--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function CTA() {
   return (
     <div className='cta'>
-      <a href="https://drive.google.com/file/d/1iLWnYz0PEjHo_se6tpwGWRXeykVnK4tg/view?usp=drive_link" target='_blank' className='btn'>Check Resume</a>
+      {/* <a href="https://drive.google.com/file/d/1iLWnYz0PEjHo_se6tpwGWRXeykVnK4tg/view?usp=drive_link" target='_blank' className='btn'>Check Resume</a> */}
       <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
